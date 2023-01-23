f4adefff09425926b582fad306810c23b88b36b5
urbao <E24094198@gs.ncku.edu.tw>
2023-01-23 14:27:54 +0800

    Upload NeoVim Setup File

---
+++ NeoVim Setup/nvim/lua/eason/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = "/"
+
+local keymap = vim.keymap
+
+--general keymaps
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split wndwos horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") --open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabclose<CR>") -- goto next tab
+keymap.set("n", "<leader>tp", ":tabclose<CR>") -- goto prev tab
+
+--plugin keymap
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- allow split window maximize & restore to original
+
+--nvime-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
