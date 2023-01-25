b97b3bad6efb1405ab47ad6d38f803f6d7d21f70
Eason Chen <87600155+urbao@users.noreply.github.com>
2023-01-25 10:41:56 +0800

    Change cursor color based on their mode

--- nvim-setup/init.lua
+++ nvim-setup/init.lua
@@ -11,5 +11,10 @@ require("eason.plugins.lspconfig")
 require("eason.plugins.completion")
 require("eason.plugins.telescope")
 
-vim.api.nvim_set_hl(0, "Cursor", {fg='grey7', bg='MediumSpringGreen'})
-vim.opt.guicursor = 'a:block-Cursor,a:blinkon100'
+-- Cursor config
+vim.api.nvim_set_hl(0, "Cursor_Insert", {fg='Grey7', bg='MediumSpringGreen' }) -- insert mode color
+vim.api.nvim_set_hl(0, "Cursor_Normal", {fg='Grey7', bg='SteelBlue1'}) -- normal mode color
+vim.api.nvim_set_hl(0, "Cursor_Visual", {fg='Grey7', bg='MediumOrchid'}) -- visual mode color
+vim.api.nvim_set_hl(0, "Cursor_Command", {fg='Grey7', bg='Tan'}) -- command mode color
+vim.opt.guicursor = 'n:block-Cursor_Normal,c-ci-cr:block-Cursor_Command,i-sm-o:block-Cursor_Insert,v-ve-r:block-Cursor_Visual,a:blinkon100'
+
