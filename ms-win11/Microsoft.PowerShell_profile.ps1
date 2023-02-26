671e9d37adf378ca34116bb283134f512bfc74dd
urbao <E24094198@gs.ncku.edu.tw>
2023-02-26 23:29:46 +0800

    Upload microsoft-windows11 powershell config file

---
+++ ms-win11/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,40 @@
+# oh-my-posh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+
+# Terminal-Icons conifgure
+Import-Module Terminal-Icons
+# PSReadline
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionView ListView
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Alias Part
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias grep findstr
+Set-Alias -Name top -Value btop
+Set-Alias -Name his -Value history
+
+# Utilities
+function which($command){
+        Get-Command -Name $command -ErrorAction SilentlyContinue|
+                Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function rmrf {
+     <#
+        .DESCRIPTION
+        Deletes the specified file or directory.
+
+        .PARAMETER target
+        Target file or directory to be deleted.
+
+        .NOTES
+        This is an equivalent command of "rm -rf" in Unix-like systems.
+        #>
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$Target
+    )
+    Remove-Item -Recurse -Force $Target
+}
